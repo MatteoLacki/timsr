26a70ab2eadfb649c27b5cef112b3d72534f1d23
MatteoLacki <matteo.lacki@gmail.com>
2020-12-06 13:17:23 +0100

    ready for submission?

--- examples/get_data.R
+++ examples/get_data.R
@@ -1,24 +1,3 @@
-# TimsR
-
-A data science friendly data access to timsTOF Pro mass spectrometry data from R.
-
-### Requirements
-
-TimsPy should work on Linux, Windows (sadly), and macOS.
-TimsPy is open-source, but as it uses OpenTIMS under the hood, it comes with similar restrictions concerning the time of flight to mass to charge and scan number to inverse ion mobility conversions [(check out for details here)](https://github.com/michalsta/opentims).
-
-
-### What can you expect?
-
-Simple way to get data out of results collected with your Bruker timsTOF Pro from R.
-This definitely ain't no rocket science, but is pretty useful!
-The data is reported in stored in `data.table` objects, that are the only thing `R` has to actually work meaningfully with big data sets.
-Before using `TimsR`, it makes a lot of sense to [study this package](https://cran.r-project.org/web/packages/data.table/vignettes/datatable-intro.html).
-A helpful cheatsheet is available [here](https://raw.githubusercontent.com/rstudio/cheatsheets/master/datatable.pdf).
-
-With that out of the way, it's all as easy as shown below:
-
-```R
 library(timsr)
 
 path = 'path/to/your/data.d'
@@ -34,9 +13,6 @@ if(accept_Bruker_EULA_and_on_Windows_or_Linux){
 } else {
     all_columns = c('frame','scan','tof','intensity','retention_time')
 }
-# NOTE: if you already did download the 'so' or 'dll', simply use
-# setup_bruker_so(path_to_bruker_dll) 
-# directly with the proper path to it.
 
 D = TimsR(path) # get data handle
 
@@ -136,73 +112,4 @@ D[1:100]
 # All MS1 frames, but one at a time:
 for(fr in MS1(D)) print(D[fr, all_columns])
 # Of course, it's better to use lapply or mclapply to speed up things.
-```
 
-### Basic plotting
-You can plot the overview of your experiment.
-Continuing on the previous example:
-
-```R
-D.plot_TIC()
-```
-![](https://github.com/MatteoLacki/timsr/blob/main/ms1_ms2_intensities.png "TIC per frame")
-<!-- 
-```R
-D.plot_peak_counts()
-```
-![](https://github.com/MatteoLacki/timspy/blob/master/ms1ms2peak_counts.png "TIC per frame")
-
-```R
-D.plot_intensity_given_mz_inv_ion_mobility()
-```
-![](https://github.com/MatteoLacki/timspy/blob/master/ms1_heatmap.png "TIC per frame")
- -->
-### Vaex support?
-
-Unfortunately, there is yet no equivalent of vaex on R.
-But we are monitoring the developments of somewhat similar `dtplyr::lazy_dt` and will possibly encourage its usage once it is tested.  
-
-
-### Plans
-* specialized methods for DDA experiments
-* going fully open-source with scan2inv_ion_mobility and tof2mz
-
-
-### Installation
-
-```{bash}
-pip install timspy
-```
-or for devel version:
-```{bash}
-pip install -e git+https://github.com/MatteoLacki/timspy/tree/devel
-```
-or with git:
-```{bash}
-git clone https://github.com/MatteoLacki/timspy
-cd timspy
-pip install -e .
-```
-
-To install vaex support, use
-```{bash}
-pip install timspy[vaex]
-```
-or install add in additional modules
-```{bash}
-pip install vaex-core vaex-hdf5
-```
-which seems much less of a hastle than figuring out how pypi internals work.
-
-### API documentation
-
-Please [visit our documentation page](https://matteolacki.github.io/timspy/index.html).
-
-### Too bloat?
-
-We have a simpler module too.
-Check out our [opentimsr package](https://github.com/michalsta/opentims).
-
-Best wishes,
-
-Matteo Lacki & Michal (Startrek) Startek
